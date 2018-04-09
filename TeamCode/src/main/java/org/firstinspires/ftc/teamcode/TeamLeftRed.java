aa23452669a0ef438f323e9277dd8d4b77261ae4
DeanNoreen <920780950@qq.com>
2018-04-09 17:02:40 +0800

    some changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamLeftRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamLeftRed.java
@@ -35,6 +35,7 @@ import android.view.View;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.TouchSensor;
@@ -49,18 +50,61 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import java.util.Locale;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.VuMarkInstanceId;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
-
+/**
+ * This 2016-2017 OpMode illustrates the basics of using the Vuforia localizer to determine
+ * positioning and orientation of robot on the FTC field.
+ * The code is structured as a LinearOpMode
+ *
+ * Vuforia uses the phone's camera to inspect it's surroundings, and attempt to locate target images.
+ *
+ * When images are located, Vuforia is able to determine the position and orientation of the
+ * image relative to the camera.  This sample code than combines that information with a
+ * knowledge of where the target images are on the field, to determine the location of the camera.
+ *
+ * This example assumes a "diamond" field configuration where the red and blue alliance stations
+ * are adjacent on the corner of the field furthest from the audience.
+ * From the Audience perspective, the Red driver station is on the right.
+ * The two vision target are located on the two walls closest to the audience, facing in.
+ * The Stones are on the RED side of the field, and the Chips are on the Blue side.
+ *
+ * A final calculation then uses the location of the camera on the robot to determine the
+ * robot's location and orientation on the field.
+ *
+ * @see VuforiaLocalizer
+ * @see VuforiaTrackableDefaultListener
+ * see  ftc_app/doc/tutorial/FTC_FieldCoordinateSystemDefinition.pdf
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list.
+ *
+ * IMPORTANT: In order to use this OpMode, you need to obtain your own Vuforia license key as
+ * is explained below.
+ */
 
 @Autonomous(name="TeamLeftRed", group ="Concept")
 //@Disabled
 public class TeamLeftRed extends LinearOpMode {
-
-    public static final String TAG = "Vuforia VuMark Sample";
     private ElapsedTime runtime = new ElapsedTime();
+    public static final String TAG = "Vuforia VuMark Sample";
     OpenGLMatrix lastLocation = null;
-
+    /**
+     * {@link #vuforia} is the variable we will use to store our instance of the Vuforia
+     * localization engine.
+     */
 
     VuforiaLocalizer vuforia;
     TouchSensor touchSensor;
@@ -192,7 +236,7 @@ public class TeamLeftRed extends LinearOpMode {
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
 
-        parameters.vuforiaLicenseKey = "AXQcjoz ///// AAABmZHq2ALQ5EDvr0pzYI / 5xQECaIBP91rdgvvcHBhGafOACEo + OAVegw1I17Nerx7pn1DrJUPHJpMfw3bQDeC7m6G / IqWOb1348XHM1HjY1tspPk7koyhWM4DKCQKfvWZZfvZ0EcZeXY4eJ5I / Ytm7o + yZTGQ50FcAbqYac0yL7M / iifDK8NYnDOaEs0bKm9gYp6TJvLblJBPqjRoGfZU288tqMT2ZxiIaqY8n4EsM0t3OuJh8aKWZF9qGJ1I879nu2MCbERZr1akfHR1uimsrgKHl05BbZmcXrIYjGgWD3BirUAU26fqyUraTtD2ms1EV1XwVTwnrhQdzxbOaO056NFgNv1HmTxQSoJ28qIuTD + F3";
+        parameters.vuforiaLicenseKey = "JAX5jFWu6OruN0EVesmSeo+m8FKtw9HexPxtE4G2EWjPx/kFe5dcd7oTi0ZYOUENJIr/bP+de6Iz3X12Nv46jRn07ywhorHvSA+FJbqZxLOxsr1ZxlEuBfH45s7K0woMUQjTGzQTcEozTRld/zAxWf9vyShez7WrjkHqJQmsYeuWLeheKvWi/Ncm60lyYHYFTzctYYf2vx4pYWR6z+N/ra26Jj0bNXGLYqKUzmjiatOmwVzXW04n/jp8p/JKljzT5N4SDgYrT7wEJGpmCX6YvWbh/1MLR2fGZ4lUHyYVE7v7BW/ZvOJNf8DpEWVQFVuvcyKD61Q5ft2557nuJMahsI";
 
 
         parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;//表示调用后置摄像头；
@@ -225,10 +269,10 @@ public class TeamLeftRed extends LinearOpMode {
         Rightrear = hardwareMap.get(DcMotor.class, "Rightrear");
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
-        Leftfront.setDirection(DcMotor.Direction.REVERSE);
+        Leftfront.setDirection(DcMotor.Direction.FORWARD);
         Leftrear.setDirection(DcMotor.Direction.FORWARD);
         Rightfront.setDirection(DcMotor.Direction.REVERSE);
-        Rightrear.setDirection(DcMotor.Direction.FORWARD);
+        Rightrear.setDirection(DcMotor.Direction.REVERSE);
         telemetry.addData(">", "按 Play 就开始");
         telemetry.update();
 
@@ -322,33 +366,33 @@ public class TeamLeftRed extends LinearOpMode {
 /////////////////////////////////////////////////////////////////////////////////////////////// Automatic /////////////////////////////////////////////////////////////////
             if (runtime.equals(30)) {
                 if (sensorColor.red() > sensorColor.blue()) {
+                    Right(0.6);
+                    sleep(40);
                     Shunshi(0.6);
                     sleep(150);
                     Nishi(0.6);
                     sleep(150);
-                    Left(0.6);
-                    sleep(30);
                 }
                 if (sensorColor.red() > sensorColor.blue()) {
+                    Right(0.6);
+                    sleep(40);
                     Nishi(0.6);
                     sleep(150);
                     Shunshi(0.6);
                     sleep(150);
-                    Left(0.6);
-                    sleep(30);
                 }
                 if (vuMark == RelicRecoveryVuMark.LEFT) {
                     Forward(0.8);
-                    sleep(200);
+                    sleep(100);
                     Left(0.8);
                     sleep(100);
-                    Forward(0.8);
+                    Forward(0.6);
                     sleep(50);
                     Release(0.40, 0.00);
                 }
                 if (vuMark == RelicRecoveryVuMark.CENTER) {
                     Forward(0.8);
-                    sleep(200);
+                    sleep(100);
                     Left(0.8);
                     sleep(150);
                     Forward(0.8);
@@ -358,7 +402,7 @@ public class TeamLeftRed extends LinearOpMode {
 
                 if (vuMark == RelicRecoveryVuMark.RIGHT) {
                     Forward(0.8);
-                    sleep(200);
+                    sleep(100);
                     Left(0.8);
                     sleep(200);
                     Forward(0.8);
@@ -369,7 +413,6 @@ public class TeamLeftRed extends LinearOpMode {
                 sleep(300);
                 Shunshi(0.6);
                 sleep(100);
-                Forward(0.8);
                 switch ("Touch") {
                     case "Is Pressed":
                         while (counter < 2) {
@@ -546,7 +589,6 @@ public class TeamLeftRed extends LinearOpMode {
 
             else {
                 telemetry.addData("VuMark", "not visible");
-
                 telemetry.addData("Status", "Run Time: " + runtime.toString());
                 // telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
             }
