30cab08fd2eee54c86457e1bbde9d4e184902263
DeanNoreen <920780950@qq.com>
2018-04-29 12:02:16 +0800

    spelling mistake:grvty to gravity

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/SensorBNO055IMU.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/SensorBNO055IMU.java
@@ -155,7 +155,7 @@ public class SensorBNO055IMU extends LinearOpMode
                 });
 
         telemetry.addLine()
-            .addData("grvty", new Func<String>() {
+            .addData("gravity", new Func<String>() {
                 @Override public String value() {
                     return gravity.toString();
                     }
